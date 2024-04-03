36bc5c28d4151af302ddc64d1f581d662d0048f0
welcomeglory <godbu2021@naver.com>
2024-04-03 12:10:23 +0900

    init

---
+++ springboot_sjy_hello3/src/main/java/edu/ex/SpringbootSjyHello3Application.java
@@ -0,0 +1,13 @@
+package edu.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSjyHello3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSjyHello3Application.class, args);
+	}
+
+}
---
+++ springboot_sjy_hello3/src/test/java/edu/ex/SpringbootSjyHello3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSjyHello3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
