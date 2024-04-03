be413eee3e0926678a32e9370d9a561705691a2a
welcomeglory <godbu2021@naver.com>
2024-04-03 16:34:46 +0900

    add

--- springboot_sjy_hello3/src/main/java/edu/ex/controller/HomeController.java
+++ springboot_sjy_hello3/src/main/java/edu/ex/controller/HomeController.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
 	@GetMapping("/home")
 	public String home() {
 		System.out.println("home()..");
 		
 		return "home";
-	}
+	}	
 	
 	@GetMapping("/home1")
 	public String home1() {
