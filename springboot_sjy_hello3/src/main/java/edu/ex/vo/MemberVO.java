d492a15b873cf8e769a449afbc6ee8e684a1f719
welcomeglory <godbu2021@naver.com>
2024-04-04 16:36:45 +0900

    add2

--- springboot_sjy_hello3/src/main/java/edu/ex/vo/MemberVO.java
+++ springboot_sjy_hello3/src/main/java/edu/ex/vo/MemberVO.java
@@ -1,14 +1,18 @@
 package edu.ex.vo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Builder
 public class MemberVO {
 	private String name;
 	private String id;
