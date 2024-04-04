4addfbc2003db7da2b288a4530b2b37ef2c646e2
welcomeglory <godbu2021@naver.com>
2024-04-04 16:41:05 +0900

    toStringtest

--- springboot_sjy_hello3/src/test/java/edu/ex/vo/MemberVOTest.java
+++ springboot_sjy_hello3/src/test/java/edu/ex/vo/MemberVOTest.java
@@ -9,7 +9,7 @@ class MemberVOTest {
 	@Test
 	void testMemberVO() {
 		MemberVO memberVO = new MemberVO();
-		System.out.println(memberVO);
+//		System.out.println(memberVO);
 		
 		memberVO.setAge(10);
 		memberVO.setName("홍길동");
@@ -19,10 +19,12 @@ class MemberVOTest {
 		assertEquals(memberVO.getAge(), 10);
 		assertEquals(memberVO.getName(), "홍길동");
 		assertTrue(memberVO.getName().equals("홍길동"));
-
-
-
-
 	}
-
+	@Test
+	void testMemberArgs() {
+		MemberVO memberVO = 
+				new MemberVO("홍길동","abcd","1234","email@email",10);
+		
+		System.out.println(memberVO);
+	}
 }
