41a4ae1a2d01c94242f5f97d823a722b8afefc7b
welcomeglory <godbu2021@naver.com>
2024-04-05 11:15:52 +0900

    path/rectangle

--- springboot_sjy_hello3/src/main/java/edu/ex/controller/PathController.java
+++ springboot_sjy_hello3/src/main/java/edu/ex/controller/PathController.java
@@ -33,4 +33,22 @@ public class PathController {
 
 		return "student";
 	}
+	//http://localhost:8282/path/circle/10
+	@RequestMapping("/circle/{radius}")
+	public String getcircle(@PathVariable double radius, Model model) {
+		System.out.println("getcircle()..");
+
+		model.addAttribute("circle", new Circle(radius));
+
+		return "/shape/circle";
+	}
+	//http://localhost:8282/path/rectangle/10/10
+		@RequestMapping("/rectangle/{width}/{height}")
+		public String getrectangle(@PathVariable double width, @PathVariable double height,Model model) {
+			System.out.println("getrectangle()..");
+
+			model.addAttribute("rec", new Rectangle(width,height));
+
+			return "/shape/rectangle";
+		}
 }
