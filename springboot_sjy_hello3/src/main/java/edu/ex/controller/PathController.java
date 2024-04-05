a3334008d6218c37d71d8acb217c31189fe0289b
welcomeglory <godbu2021@naver.com>
2024-04-05 10:46:42 +0900

    path

---
+++ springboot_sjy_hello3/src/main/java/edu/ex/controller/PathController.java
@@ -0,0 +1,36 @@
+package edu.ex.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.ex.vo.Circle;
+import edu.ex.vo.Grade;
+import edu.ex.vo.Rectangle;
+
+@Controller 
+@RequestMapping("/path")
+public class PathController {	
+	
+	//http://localhost:8282/path/student/10
+	@RequestMapping("/student/{studentId}")
+	public String getstudent(@PathVariable String studentId, Model model) {
+		System.out.println("getstudent()..");
+
+		model.addAttribute("studentId", studentId);
+
+		return "student";
+	}
+}
